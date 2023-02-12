ce25e7cc3497047523efc86fba7d4109d582b4e7
Leon simintei <mallfs@icloud.com>
2023-02-12 19:48:08 +0300

    assignment complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class Dog
+    def name=(name)
+       @name = name
+    end
+    def name
+      @name
+    end
 
+    def breed=(breed)
+       @breed = breed
+    end
+    def breed
+       @breed
+    end
+    #binding.pry
 end
+ 
